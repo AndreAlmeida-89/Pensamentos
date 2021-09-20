ff99b2a5d939ea26fe8776e1ff56da79d24437d4
André Almeida <andre.almeida@iteris.com.br>
2021-09-20 11:51:15 -0300

    Initial Commit

---
+++ Pensamentos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pensamentos
+//
+//  Created by André Felipe de Sousa Almeida - AAD on 20/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
